831b7623eb89fda45a439ffa97c820f588558638
jonas breen <jonasebreen@gmail.com>
2024-12-13 20:22:53 -0500

    seeker working!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,19 @@
 use std::{
     fs::File,
     io::{self, BufReader},
-    sync::mpsc::{channel, Receiver}, time::Duration,
+    sync::mpsc::{channel, Receiver},
+    time::Duration,
 };
 
 use fltk::{
-    button::Button, draw, enums::{Color, FrameType}, frame::Frame, prelude::*, valuator::{Slider, SliderType}, window::Window
+    app::sleep,
+    button::Button,
+    draw,
+    enums::{Color, FrameType},
+    frame::Frame,
+    prelude::*,
+    valuator::{Slider, SliderType},
+    window::Window,
 };
 use fltk_theme::colors::html;
 use rodio::{Decoder, OutputStream, Sink, Source};
@@ -15,6 +23,7 @@ enum PlayerMessage {
     Stop,
     Seek(f64),
     Quit,
+    GetPos(Box<dyn FnMut(f64) + Send>),
 }
 
 #[tokio::main]
@@ -38,9 +47,10 @@ async fn main() -> io::Result<()> {
     seeker.set_frame(FrameType::RFlatBox);
     seeker.set_color(Color::from_u32(0x868db1));
     let seeker_tx = tx.clone();
-    seeker.set_callback(move |s|{
-        // println!("seeker value: {}", s.value());
-        seeker_tx.send(PlayerMessage::Seek(s.value())).unwrap();
+    seeker.set_callback(move |s| {
+        seeker_tx
+            .send(PlayerMessage::Seek(s.value()))
+            .expect("failed to send Seek message");
         fltk::app::redraw();
     });
 
@@ -56,6 +66,31 @@ async fn main() -> io::Result<()> {
         );
     });
 
+    let seeker_pos_tx = tx.clone();
+    let (seeker_tx, seeker_rx) = channel::<f64>();
+    tokio::spawn(async move {
+        for _ in 0.. {
+            let tx_clone = seeker_tx.clone();
+            let msg: PlayerMessage = PlayerMessage::GetPos(Box::new(move |pos| {
+                tx_clone
+                    .send(pos)
+                    .expect("failed to send position to seeker");
+            }));
+            seeker_pos_tx
+                .send(msg)
+                .expect("failed to send get_pos message");
+            sleep(0.05);
+        }
+    });
+
+    tokio::spawn(async move {
+        for pos in seeker_rx {
+            seeker.set_value(pos);
+            fltk::app::awake();
+            fltk::app::redraw();
+        }
+    });
+
     win.set_color(html::Red);
     play_btn.set_frame(FrameType::NoBox);
     play_btn.set_color(html::Red);
@@ -67,36 +102,40 @@ async fn main() -> io::Result<()> {
     play_btn.set_callback(move |_| {
         frame.set_label("you clicked the button!");
         println!("clicked");
-        match play_btn_tx.send(PlayerMessage::Play) {
-            Ok(_) => {}
-            Err(e) => panic!("{}", e),
-        };
+        play_btn_tx
+            .send(PlayerMessage::Play)
+            .expect("failed to send play message");
     });
     let stop_btn_tx = tx.clone();
     stop_btn.set_callback(move |_| {
-        stop_btn_tx.send(PlayerMessage::Stop)
-            .expect("failed to send play message");
+        stop_btn_tx
+            .send(PlayerMessage::Stop)
+            .expect("failed to send stop message");
     });
+
     // kill all threads when the X button is clicked
     win.set_callback(move |_| {
-        tx.send(PlayerMessage::Quit).unwrap();
+        tx.send(PlayerMessage::Quit)
+            .expect("failed to send quit message");
         app.quit();
     });
-    app.run().unwrap();
+    app.run().expect("could not run app");
     Ok(())
 }
 
 async fn play_manager(rx: Receiver<PlayerMessage>) {
     // let stream_handle = OutputStream::try_default().unwrap();
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let sink = Sink::try_new(&stream_handle).unwrap();
+    let (_stream, stream_handle) =
+        OutputStream::try_default().expect("could not create default OutputStream");
+    let sink = Sink::try_new(&stream_handle).expect("could not create new Sink");
 
-    let file = BufReader::new(File::open("assets/test.flac").unwrap());
-    let source = Decoder::new(file).unwrap();
-    let duration = source.total_duration().unwrap();
+    let file = BufReader::new(File::open("assets/test.flac").expect("failed to load test file"));
+    let source = Decoder::new(file).expect("failed to create decoder from test file");
+    let duration = source
+        .total_duration()
+        .expect("failed to get souce duration");
     sink.append(source);
     sink.pause();
-
 
     for msg in rx {
         match msg {
@@ -111,11 +150,17 @@ async fn play_manager(rx: Receiver<PlayerMessage>) {
             PlayerMessage::Seek(place) => {
                 println!("seeking {}", place);
                 let pos = place * duration.as_secs_f64();
-                sink.try_seek(Duration::from_secs_f64(pos)).unwrap();
+                sink.try_seek(Duration::from_secs_f64(pos))
+                    .expect("could not seek");
             }
             PlayerMessage::Quit => {
                 return;
-            },
+            }
+            PlayerMessage::GetPos(mut call_back) => {
+                let pos = sink.get_pos();
+                let f64_pos = pos.as_secs_f64() / duration.as_secs_f64();
+                call_back(f64_pos);
+            }
         }
     }
 }
